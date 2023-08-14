c8030a085515b310f2498173d4a8e0e15d7e4a70
George Arbanas <george.arbanas@icloud.com>
2023-08-14 17:49:49 +0900

    added activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,24 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    @activity = Activity.all
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.create(activity_params)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:location, :description, :hourly_rate)
+  end
 end
