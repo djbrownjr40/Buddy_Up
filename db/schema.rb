9e02bc61cb012325215e2951fdb027e438b2e345
Noemi Radu <noemiradu12@gmail.com>
2023-08-15 13:39:23 +1000

    Fixed the preferences column's name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_031926) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_033336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_031926) do
     t.string "first_name"
     t.string "last_name"
     t.string "gender"
-    t.string "prefefrences"
+    t.string "preferences"
     t.integer "age"
     t.integer "user_rating"
     t.index ["email"], name: "index_users_on_email", unique: true
