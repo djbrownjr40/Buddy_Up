0b3ad5b26a0d6790cbd8590f27b9ccd48e0b32ee
George Arbanas <george.arbanas@icloud.com>
2023-08-16 16:51:52 +0900

    adjusted bookings params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,6 +23,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:date, :user_id, :activity_id)
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :activity_id)
   end
 end
