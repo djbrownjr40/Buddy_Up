3a44ee4f33b4974c0f76cab2044bd6725097f312
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-08-17 15:39:34 +0900

    create activity functional

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,17 +10,20 @@ class ActivitiesController < ApplicationController
     @booking = Booking.new
   end
 
-  def new
-    @activity = Activity.new
-  end
+  # def new
+  #   @activity = Activity.new
+  # end
 
   def create
     @activity = Activity.create(activity_params)
+    @activity.user = current_user
+    @activity.save
+    redirect_to activity_path(@activity)
   end
 
   private
 
   def activity_params
-    params.require(:activity).permit(:location, :description, :hourly_rate)
+    params.require(:activity).permit(:location, :description, :hourly_rate, :photo)
   end
 end
