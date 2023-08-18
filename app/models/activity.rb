ef12b7ba42ad314389c1d3b7af668b8f10375761
Noemi Radu <noemiradu12@gmail.com>
2023-08-18 13:46:25 +1000

    reviews star's and comment displaying

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,18 @@
 class Activity < ApplicationRecord
   has_many :bookings
+  has_many :reviews
   belongs_to :user
-  validates :location, :hourly_rate, :description, presence: true
   has_one_attached :photo
+
+  validates :location, :hourly_rate, :description, presence: true
+
+  def average_rating
+    return 0 if reviews.nil? || reviews.empty?
+
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    sum / reviews.length
+  end
 end
